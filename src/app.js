9b3198c55b0fc9f5b4dd662fd3073675102e8391
Roberto Solares <betosolaresgar@gmail.com>
2021-07-03 16:56:29 -0600

    Add camara view and flip button

--- src/app.js
+++ src/app.js
@@ -1,27 +1,80 @@
 import { registerRootComponent } from "expo";
+import {Camera} from "expo-camera";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, {useEffect, useState} from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-const App = () => (
-  <View style={styles.container}>
-    <Text style={styles.text}>Hello World!</Text>
-    <StatusBar style="auto" />
-  </View>
-);
+const App = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return(
+      <View style={styles.container}>
+        <Text style={styles.text}>No permission to camera granted.</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#9bd1cd",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+
+  camera: {
+    flex: 1,
+  },
+
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
   },
 
   text: {
-    color: "#121212",
     fontSize: 18,
-  },
+    color: 'white',
+  }
 });
 
 export default registerRootComponent(App);
