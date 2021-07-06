5f4020e7e30706ed93d8775bf9f66210947f54e1
Roberto Solares <betosolaresgar@gmail.com>
2021-07-05 18:37:03 -0600

    Finish camera view

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import { registerRootComponent } from "expo";
 import { Camera } from "expo-camera";
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { SafeAreaView, StyleSheet, View } from "react-native";
 
 import {
+  CustomButton,
   CustomText,
   IconifyButton,
   LinkText,
@@ -12,16 +13,54 @@ import {
 } from "./components";
 
 const App = () => {
+  const cameraReference = useRef();
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const [preview, setPreview] = useState(false);
 
   useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === "granted");
-    })();
+    askPermission();
   }, []);
+
+  const askPermission = async () => {
+    const { status } = await Camera.requestPermissionsAsync();
+    setHasPermission(status === "granted");
+  };
+
+  const handleType = () => {
+    setType(
+      type === Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back
+    );
+  };
+
+  const handleFlash = () => {
+    setFlash(
+      flash === Camera.Constants.FlashMode.off
+        ? Camera.Constants.FlashMode.torch
+        : Camera.Constants.FlashMode.off
+    );
+  };
+
+  const snap = async () => {
+    if (cameraReference.current) {
+      const options = { quality: 1, base64: true };
+      const data = await cameraReference.current.takePictureAsync(options);
+      const source = data.base64;
+
+      if (source) {
+        await cameraReference.current.pausePreview();
+        setPreview(true);
+      }
+    }
+  };
+
+  const cancelPreview = async () => {
+    await cameraReference.current.resumePreview();
+    setPreview(false);
+  };
 
   if (hasPermission === null) {
     return <View />;
@@ -49,10 +88,7 @@ const App = () => {
           color={"#3366BB"}
           textAlign={"center"}
           content={"Give access"}
-          onPress={async () => {
-            const { status } = await Camera.requestPermissionsAsync();
-            setHasPermission(status === "granted");
-          }}
+          onPress={askPermission}
           h4
         />
         <StatusBar style="auto" />
@@ -63,79 +99,94 @@ const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Camera
+        ref={cameraReference}
         style={styles.camera}
         type={type}
         flashMode={flash}
         autoFocus={"on"}
       >
         <View style={styles.uiContainer}>
-          <View style={styles.topControls}>
-            <IconifyButton
-              style={styles.iconButton}
-              icon={"account-circle"}
-              iconSize={34}
-              color={"#FEFEFE"}
-              onPress={() => {}}
-            />
-            <ToggleButton
-              icon={"camera-front"}
-              toggledIcon={"camera-rear"}
-              iconSize={34}
-              color={"#FEFEFE"}
-              styles={styles.iconButton}
-              onPress={() => {
-                setType(
-                  type === Camera.Constants.Type.back
-                    ? Camera.Constants.Type.front
-                    : Camera.Constants.Type.back
-                );
-              }}
-            />
-            <ToggleButton
-              icon={"flash-off"}
-              toggledIcon={"flash"}
-              iconSize={34}
-              color={"#FEFEFE"}
-              styles={styles.iconButton}
-              onPress={() => {
-                setFlash(
-                  flash === Camera.Constants.FlashMode.off
-                    ? Camera.Constants.FlashMode.torch
-                    : Camera.Constants.FlashMode.off
-                );
-              }}
-            />
-            <IconifyButton
-              style={styles.iconButton}
-              icon={"nut"}
-              iconSize={34}
-              color={"#FEFEFE"}
-              onPress={() => {}}
-            />
-          </View>
-          <View style={styles.bottomControls}>
-            <IconifyButton
-              style={styles.iconButton}
-              icon={"upload"}
-              iconSize={34}
-              color={"#FEFEFE"}
-              onPress={() => {}}
-            />
-            <IconifyButton
-              style={styles.iconButton}
-              icon={"circle-double"}
-              iconSize={68}
-              color={"#FEFEFE"}
-              onPress={() => {}}
-            />
-            <IconifyButton
-              style={styles.iconButton}
-              icon={"circle-edit-outline"}
-              iconSize={34}
-              color={"#FEFEFE"}
-              onPress={() => {}}
-            />
-          </View>
+          {!preview && (
+            <>
+              <View style={styles.topControls}>
+                <IconifyButton
+                  style={styles.iconButton}
+                  icon={"account-circle"}
+                  iconSize={34}
+                  color={"#FEFEFE"}
+                  onPress={() => {}}
+                />
+                <ToggleButton
+                  icon={"camera-front"}
+                  toggledIcon={"camera-rear"}
+                  iconSize={34}
+                  color={"#FEFEFE"}
+                  styles={styles.iconButton}
+                  onPress={handleType}
+                />
+                <ToggleButton
+                  icon={"flash-off"}
+                  toggledIcon={"flash"}
+                  iconSize={34}
+                  color={"#FEFEFE"}
+                  styles={styles.iconButton}
+                  onPress={handleFlash}
+                />
+                <IconifyButton
+                  style={styles.iconButton}
+                  icon={"nut"}
+                  iconSize={34}
+                  color={"#FEFEFE"}
+                  onPress={() => {}}
+                />
+              </View>
+              <View style={styles.bottomControls}>
+                <IconifyButton
+                  style={styles.iconButton}
+                  icon={"upload"}
+                  iconSize={34}
+                  color={"#FEFEFE"}
+                  onPress={() => {}}
+                />
+                <IconifyButton
+                  style={styles.iconButton}
+                  icon={"circle-double"}
+                  iconSize={68}
+                  color={"#FEFEFE"}
+                  onPress={snap}
+                />
+                <IconifyButton
+                  style={styles.iconButton}
+                  icon={"circle-edit-outline"}
+                  iconSize={34}
+                  color={"#FEFEFE"}
+                  onPress={() => {}}
+                />
+              </View>
+            </>
+          )}
+          {preview && (
+            <>
+              <View style={styles.topControls}>
+                <IconifyButton
+                  style={styles.iconButton}
+                  icon={"close-thick"}
+                  iconSize={34}
+                  color={"#FEFEFE"}
+                  onPress={cancelPreview}
+                />
+              </View>
+              <View style={styles.bottomControls}>
+                <CustomButton
+                  backgroundColor={"#F7F401"}
+                  color={"#000102"}
+                  size={"big"}
+                  tittle={"Find Recepie"}
+                  onPress={() => {}}
+                />
+              </View>
+            </>
+          )}
         </View>
       </Camera>
     </SafeAreaView>
