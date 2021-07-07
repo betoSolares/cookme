832c165be4f088ee82522273d3f45842c82ea114
Roberto Solares <betosolaresgar@gmail.com>
2021-07-07 15:50:04 -0600

    Add list item components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as CustomButton } from "./button";
 export { default as IconifyButton } from "./iconifybutton";
+export { NormalListItem, RemovableListItem } from "./listitems";
 export { CustomText, LinkText } from "./text";
 export { default as ToggleButton } from "./togglebutton";
---
+++ src/components/listitems.js
@@ -0,0 +1,53 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+import { IconifyButton } from ".";
+import { CustomText } from "./text";
+
+export const NormalListItem = ({ text, backgroundColor, color }) => (
+  <View style={[styles.item, { backgroundColor }]}>
+    <CustomText p content={text} color={color} textAlign={"auto"} />
+  </View>
+);
+
+export const RemovableListItem = ({
+  text,
+  backgroundColor,
+  color,
+  onRemove,
+}) => (
+  <View style={[styles.item, { backgroundColor }]}>
+    <CustomText p content={text} color={color} textAlign={"auto"} />
+    <IconifyButton
+      icon={"delete"}
+      iconSize={24}
+      color={color}
+      onPress={onRemove}
+    />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+});
+
+NormalListItem.propTypes = {
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string.isRequired,
+};
+
+RemovableListItem.propTypes = {
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string.isRequired,
+  onRemove: PropTypes.func.isRequired,
+};
