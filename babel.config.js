8fe54f71b2b688daeab2f19c9320685792f8e167
Roberto Solares <betosolaresgar@gmail.com>
2021-07-08 15:50:34 -0600

    Add a way to get environment variables

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
+
+  const presets = ["babel-preset-expo"];
+  const plugins = ["module:react-native-dotenv"];
+
   return {
-    presets: ["babel-preset-expo"],
+    presets,
+    plugins
   };
 };
