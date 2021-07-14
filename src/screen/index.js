00e3eea7bd8dbb389edb4931000deafffa099edb
Roberto Solares <betosolaresgar@gmail.com>
2021-07-14 12:00:51 -0600

    Add specific recepie view

--- src/screen/index.js
+++ src/screen/index.js
@@ -3,3 +3,4 @@ export { default as CameraScreen } from "./camera";
 export { default as IngredientsScreen } from "./ingredients";
 export { default as RecepiesScreen } from "./recepies";
 export { default as SettingsScreen } from "./settings";
+export { default as SpecificScreen } from "./specific";
