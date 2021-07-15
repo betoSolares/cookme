7999bcb902b6160e62a561c299729c4cec9d6a8e
Roberto Solares <betosolaresgar@gmail.com>
2021-07-14 21:34:14 -0600

    Fix linting

--- src/screen/index.js
+++ src/screen/index.js
@@ -1,7 +1,7 @@
 export { default as AccountScreen } from "./account";
 export { default as CameraScreen } from "./camera";
 export { default as IngredientsScreen } from "./ingredients";
+export { default as LogInScreen } from "./login";
 export { default as RecepiesScreen } from "./recepies";
 export { default as SettingsScreen } from "./settings";
 export { default as SpecificScreen } from "./specific";
-export { default as LogInScreen } from "./login";
